774408f93eba9b6ebc84729d7d41e068b38631f8
Adam Li <adam2392@gmail.com>
2024-05-17 17:50:02 -0700

    Fix job submission
    Signed-off-by: Adam Li <adam2392@gmail.com>

--- experiments/gencauca/submit-python-jobs.sh
+++ experiments/gencauca/submit-python-jobs.sh
@@ -13,11 +13,11 @@ training_seeds=(1 1000 2000 3000 4000 5000 6000 7000 8000 9000 10000)
 for TRAINING_SEED in "${training_seeds[@]}"
 do
   # Construct the command to run the Python script with the current training seed
-#   CMD="python3 $SCRIPT_NAME --training-seed $TRAINING_SEED"
+  CMD="python3 $SCRIPT_NAME --training-seed $TRAINING_SEED"
   
-#   # Optionally, you can use a job scheduler like `nohup` to run the command in the background
-#   # or `&` to run the command in the background
-#   nohup $CMD > output_seed_$TRAINING_SEED.log 2>&1 &
+  # Optionally, you can use a job scheduler like `nohup` to run the command in the background
+  # or `&` to run the command in the background
+  nohup $CMD > output_seed_$TRAINING_SEED.log 2>&1 &
   
   echo "Submitted job for training seed: $TRAINING_SEED"
 done
