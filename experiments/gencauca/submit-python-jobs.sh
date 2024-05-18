2eccb6a841b31f1ef6978fbdf3280cbc908688b5
Adam Li <adam2392@gmail.com>
2024-05-17 17:49:00 -0700

    Fix job submission
    Signed-off-by: Adam Li <adam2392@gmail.com>

--- experiments/gencauca/submit-python-jobs.sh
+++ experiments/gencauca/submit-python-jobs.sh
@@ -6,15 +6,18 @@ SCRIPT_NAME="02-script-collidergraph.py"
 # Change to the directory containing the script
 # cd "$SCRIPT_DIR"
 
+# Define the training seeds to match np.linspace(1, 10000, 11, dtype=int)
+training_seeds=(1 1000 2000 3000 4000 5000 6000 7000 8000 9000 10000)
+
 # Loop over the training seeds and submit a job for each seed
-for TRAINING_SEED in $(seq 1 10000 1000)
+for TRAINING_SEED in "${training_seeds[@]}"
 do
   # Construct the command to run the Python script with the current training seed
-  CMD="python3 $SCRIPT_NAME --training-seed $TRAINING_SEED"
+#   CMD="python3 $SCRIPT_NAME --training-seed $TRAINING_SEED"
   
-  # Optionally, you can use a job scheduler like `nohup` to run the command in the background
-  # or `&` to run the command in the background
-  nohup $CMD > output_seed_$TRAINING_SEED.log 2>&1 &
+#   # Optionally, you can use a job scheduler like `nohup` to run the command in the background
+#   # or `&` to run the command in the background
+#   nohup $CMD > output_seed_$TRAINING_SEED.log 2>&1 &
   
   echo "Submitted job for training seed: $TRAINING_SEED"
 done
